b04e6c310aa6ef03313cd72bb8609081a2e2adf9
test01-16 <71700094+test01-16@users.noreply.github.com>
2021-01-09 21:39:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
 # https://flask.palletsprojects.com/en/1.1.x/
 from flask import Flask,render_template,redirect
-from flask.helpers import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    #return redirect(url_for("eth_world"))
-    return {"status":200}
-    
+    return redirect("https://www.tesla.com/",code=302)
+
 @app.route('/eth')
 def eth_world():
     return render_template("eth.html")
@@ -28,3 +26,4 @@ def page_not_found(e):
 if __name__ == '__main__':
 #   app.run(host='localhost', port=8080)
   app.run(host='0.0.0.0', port=8000)
+  
